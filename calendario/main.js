74f973a622e3cb74200a5f5c7563d2e6ec56899e
Ana Laura <analaurass45@gmail.com>
2023-05-18 00:12:19 -0300

    adicionando todos os meses

--- calendario/main.js
+++ calendario/main.js
@@ -1,29 +1,75 @@
-
 const monthEl = document.querySelector(".date h1");
 const FullDate = document.querySelector(".date p");
 const daysEl = document.querySelector(".days");
+const clicked = document.querySelector(".days div")
 
-const monthIndex = new Date().getMonth();
-const lastDay = new Date(new Date().getFullYear(), monthIndex + 1, 0).getDate();
-const firstDay = new Date(new Date().getFullYear(), monthIndex, 1).getDay()-1;
+let monthIndex = new Date().getMonth();
+let yearIndex = new Date().getFullYear();
 
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+function clickedDay(){}
 
-monthEl.innerText = months[monthIndex];
-FullDate.innerText = new Date().toDateString();
-
-let days = "";
-
-for(let i = firstDay; i > 0; i--){
-    days += `<div class="empty"></div>`;
-}
-
-for(let i = 1; i < lastDay; i++){
-    if(i == new Date().getDate()){
-        days += `<div class="today">${i}</div>`;        
-    } else{
-        days += `<div>${i}</div>`;
+function monthUp() {
+    monthIndex += 1;
+    if (monthIndex > 11) {
+        monthIndex = 0;
+        yearIndex += 1;    
     }
+    calender();
 }
 
-daysEl.innerHTML = days;
+function monthDown() {
+    monthIndex -= 1;
+    if(monthIndex < 0){
+        monthIndex = 11;
+        yearIndex -= 1;
+    }
+    calender();
+}
+
+console.log(clicked)
+
+function calender(){
+    const lastDay = new Date(yearIndex, monthIndex + 1, 0).getDate();
+    let firstDay = new Date(yearIndex, monthIndex, 1).getDay() - 1;
+    let today = new Date(yearIndex, monthIndex, new Date().getDate());
+
+    if (firstDay < 0){
+        firstDay = 6
+    }
+    
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    
+    monthEl.innerText = months[monthIndex];
+    FullDate.innerText = today.toDateString();
+    
+    let days = "";
+    
+    for (let i = firstDay; i > 0; i--) {
+      days += `<div class="empty"></div>`;
+    }
+    
+    for (let i = 1; i <= lastDay; i++) {
+      if (i == new Date().getDate()) {
+        days += `<div class="today">${i}</div>`;
+      } else {
+        days += `<div>${i}</div>`;
+      }
+    }
+    
+    daysEl.innerHTML = days;
+}
+
+calender();
